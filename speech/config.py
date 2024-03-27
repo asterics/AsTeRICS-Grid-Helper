21b8709f55bb56e10d55565a92ff4eb8f201a3e3
Benjamin Klaus <klaus@technikum-wien.at>
2024-03-27 12:06:07 +0100

    using pytts speech provider by default

--- speech/config.py
+++ speech/config.py
@@ -1,8 +1,8 @@
-# import provider_pytts_playing
+import provider_pytts_playing
 # import provider_msazure_playing
 # import provider_msazure_data
 # import provider_piper_data
-import provider_mimic3_data
+# import provider_mimic3_data
 
-speechProviderList = [provider_mimic3_data]
+speechProviderList = [provider_pytts_playing]
 cacheData = True
