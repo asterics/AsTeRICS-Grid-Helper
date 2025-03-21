e74f398d5e24517e6be61a49ab034c900982a5f1
Benjamin Klaus <benjamin@asterics-foundation.org>
2025-03-21 15:28:54 +0100

    #506 made it possible to login with syncConfig passed by parameter, return different result values for login

--- speech/config.py
+++ speech/config.py
@@ -1,8 +1,9 @@
-import provider_pytts_playing
+# import provider_pytts_playing
 # import provider_msazure_playing
-# import provider_msazure_data
+import provider_msazure_data
+import provider_elevenlabs_data
 # import provider_piper_data
 # import provider_mimic3_data
 
-speechProviderList = [provider_pytts_playing]
+speechProviderList = [provider_msazure_data, provider_elevenlabs_data]
 cacheData = True
